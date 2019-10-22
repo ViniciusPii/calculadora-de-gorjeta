6425b5898d1a2be1f68bcb05b60c8638e9d8a557
Pii <pii@Piis-iMac.local>
2019-10-22 20:50:30 -0300

    Implementação da porcentagem Dinamica

--- App.js
+++ App.js
@@ -1,98 +1,137 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components/native';
 
 const Page = styled.View`
-  flex: 1;
   align-items: center;
+  flex: 1;
 `;
 
 const HeaderText = styled.Text`
-  font-size: 30px;
-  margin-top: 50px;
+  color: #646464;
+  font-size: 25px;
+  margin-top: 40px;
 `;
 
 const Input = styled.TextInput`
-  width: 80%;
-  height: 50px;
-  font-size: 20px;
   background-color: #eee;
-  margin-top: 20px;
   border-radius: 10px;
-  padding: 10px;
-`;
-
-const CalcButton = styled.Text`
-  width: 80%;
+  font-size: 18px;
   height: 50px;
-  border-radius: 10px;
-  background-color: #009;
-  margin-top: 30px;
-  text-align: center;
-  line-height: 50px;
+  margin: 20px 0;
+  padding: 10px;
+  width: 80%;
 `;
 
-const TextButton = styled.Text`
+const Title = styled.Text`
+  color: #646464;
   font-size: 20px;
+  font-weight: bold;
+  margin-bottom: 20px;
+`;
+
+const PctArea = styled.View`
+  flex-direction: row;
+`;
+
+const PctButton = styled.Text`
+  background-color: #646464;
+  border-radius: 10px;
+  height: 40px;
+  line-height: 50px;
+  margin: 10px 10px 50px 10px;
+  text-align: center;
+  width: 40px;
+`;
+
+const PctTextButton = styled.Text`
   color: #fff;
+  font-size: 18px;
   text-transform: uppercase;
 `;
 
 const ResultArea = styled.View`
-  margin-top: 30px;
-  background-color: #eee;
-  padding: 20px;
-  justify-content: center;
   align-items: center;
-  width: 80%;
+  background-color: #eee;
   border-radius: 10px;
+  justify-content: center;
+  padding: 20px;
+  width: 80%;
+`;
+
+const ResultItem = styled.View`
+  flex-direction: row;
+  padding: 5px;
 `;
 
 const ResultItemTitle = styled.Text`
-  font-size: 25px;
+  color: #646464;
+  font-size: 18px;
   font-weight: bold;
 `;
 
-const ResultItem = styled.Text`
-  font-size: 20px;
-  padding: 15px;
+const ResultSubTitle = styled.Text`
+  color: #808080;
+  font-size: 18px;
+  padding-left: 10px;
 `;
+
+
 
 export default () => {
 
   const [bill, setBill] = useState('');
-  const [tip, setTip] = useState(0);
+  const [pct, setPct] = useState('');
+  const [tip, setTip] = useState('');
 
   const calc = () => {
     let nBill = parseFloat(bill);
 
     if (nBill) {
-      setTip(nBill * 0.1);
-    } else {
-      alert("Digite o valor da conta!!!");
+      setTip((pct / 100) * nBill);
     }
   }
+
+  const error = () => {
+    if (bill == '' || bill == 0) {
+      alert('bla');
+    }
+  }
+
+  useEffect(() => {
+    calc();
+  }, [pct, bill]);
 
   return (
     <Page>
       <HeaderText>Calculadora de Gorjeta</HeaderText>
       <Input
         placeholder="Quanto deu sua conta?"
-        placeholderTextColor="#000"
+        placeholderTextColor="#646464"
         keyboardType="numeric"
         value={bill}
         onChangeText={n => setBill(n)}
       />
-      <CalcButton onPress={calc}>
-        <TextButton>Calcular</TextButton>
-      </CalcButton>
-      {tip > 0 &&
+      <Title>Porcentagem da Gorjeta</Title>
+      <PctArea>
+        <PctButton onPress={() => setPct(5)}><PctTextButton>5%</PctTextButton></PctButton>
+        <PctButton onPress={() => setPct(10)}><PctTextButton>10%</PctTextButton></PctButton>
+        <PctButton onPress={() => setPct(15)}><PctTextButton>15%</PctTextButton></PctButton>
+        <PctButton onPress={() => setPct(20)}><PctTextButton>20%</PctTextButton></PctButton>
+      </PctArea>
+      {tip > 0 && bill != '' &&
         <ResultArea>
-          <ResultItemTitle>Valor da conta</ResultItemTitle>
-          <ResultItem>R$ {parseFloat(bill).toFixed(2)}</ResultItem>
-          <ResultItemTitle>Valor da gorjeta</ResultItemTitle>
-          <ResultItem>R$ {tip.toFixed(2)} (10%)</ResultItem>
-          <ResultItemTitle>Valor total</ResultItemTitle>
-          <ResultItem>R$ {(parseFloat(bill) + tip).toFixed(2)}</ResultItem>
+          <ResultItem>
+            <ResultItemTitle>Valor da Conta:</ResultItemTitle>
+            <ResultSubTitle>R$ {parseFloat(bill).toFixed(2)}</ResultSubTitle>
+          </ResultItem>
+          <ResultItem>
+            <ResultItemTitle>Valor da Gorjeta:</ResultItemTitle>
+            <ResultSubTitle>R$ {tip.toFixed(2)} ({`${pct}%`})</ResultSubTitle>
+          </ResultItem>
+          <ResultItem>
+            <ResultItemTitle>Valor Total:</ResultItemTitle>
+            <ResultSubTitle>R$ {(parseFloat(bill) + tip).toFixed(2)}</ResultSubTitle>
+          </ResultItem>
         </ResultArea>
       }
     </Page>
